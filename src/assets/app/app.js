cc6bc814fac71226e59be1a2c918c3eaccd92236
Xavier Reyes Ochoa <xareyesochoa@gmail.com>
2016-04-18 18:41:48 -0700

    intro: added animations

--- src/assets/app/app.js
+++ src/assets/app/app.js
@@ -1,1 +1,41 @@
 'use strict';
+
+$(function() {
+
+    /* ANIMATE PATH*/
+
+    $("path").each(function() {
+
+        var path = this;
+        //get length of path
+        var length = path.getTotalLength();
+
+        //starting positions
+        $(this).css({
+            "stroke-dasharray": length + ' ' + length,
+            "stroke-dashoffset": length
+        });
+
+        setTimeout(function() {
+            $(path).addClass("animate");
+        }, 100);
+
+    });
+
+    /* ANIMATE ICONS */
+
+    $("#social-icons .cell").each(function(index) {
+
+        var $icon = $(this);
+
+        setTimeout(function() {
+            $icon.addClass("animate");
+        }, 500 * index);
+
+    });
+
+    /* OVERLAY */
+    
+    $('#overlay').addClass("animate");
+
+});
