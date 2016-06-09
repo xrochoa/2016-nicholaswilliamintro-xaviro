53c37782fcff5eb56b3e9145aebbe98c25f2c228
Xavier Reyes <xareyesochoa@gmail.com>
2016-06-08 23:10:19 -0700

    shop: added old js as comments

--- src/assets/js/app/ctrl/shop.js
+++ src/assets/js/app/ctrl/shop.js
@@ -1,5 +1,31 @@
 angular.module('app').controller('shopCtrl', function($scope) {
 
+    /* ANIMATE LOGO, ICONS & FOOTER */
+
+/*    $(".img-logo, #social-icons .icon, #site-footer").each(function(index) {
+
+        $(this).addClass("animate");
+
+    });
+*/
+    /* MODAL */
+
+/*    $('.shirt').on('click', function() {
+
+        //open modal
+        $('#modal').addClass('open');
+
+        //change image
+        var index = $('.shirt').index(this);
+        //console.log(index);
+        $("#shirt-modal").html('<img src="../assets/img/shirts/' + (index + 1) + '.png">');
+
+    });*/
+
+    //close modal
+/*    $('.close-modal-button, #modal-overlay').on('click', function() {
+        $('#modal').removeClass('open');
+    });*/
 
 
 })
